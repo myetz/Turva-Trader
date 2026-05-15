ef9e3cf22a296efceedc64ffbfe08c714d981366
myetz <105242262+myetz@users.noreply.github.com>
2026-05-15 16:45:02 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://nrvcfypqknvdnmgwiuzz.supabase.co',
+  'sb_publishable_HD5bAm1Yh7muNA7pe7AKxg_c_Z1DIlA'
+)
