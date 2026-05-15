75fce1397f0fff58a17f3535531b6e97449fecbc
myetz <105242262+myetz@users.noreply.github.com>
2026-05-15 16:52:04 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  'https://nrvcfypqknvdnmgwiuzz.supabase.co',
-  'sb_publishable_HD5bAm1Yh7muNA7pe7AKxg_c_Z1DIlA'
-)
